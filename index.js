e92ba47e00cb7fad0e30b6122f034c0bd3074de2
zaak <zaak.kerstetter@gmail.com>
2022-04-25 20:55:38 -0600

    commit tree 39810adc3ecbf4c04a6d6040a142b5cfdad634c2

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Follow along with the examples here
+function sayHelloTo(firstName){
+    console.log(`"Hello, ${firstName}!"`)
+}
+sayHelloTo("Zaak");
+sayHelloTo("James");
+
+function add(x, y){
+    return x + y;
+}
+console.log(add(1, 2));
+
+function say(greeting, firstName){
+    return `${greeting}, ${firstName}!`;
+}
